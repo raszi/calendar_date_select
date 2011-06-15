122da6d0f90e59b2388e1dfab5590d2fab7c8622
KARASZI István <github@spam.raszi.hu>
2011-06-15 15:08:52 +0200

    better loaded check for stylesheets/javascripts

--- lib/calendar_date_select/includes_helper.rb
+++ lib/calendar_date_select/includes_helper.rb
@@ -1,12 +1,28 @@
 module CalendarDateSelect::IncludesHelper
+  def calendar_date_select_stylesheets_loaded?
+    @cds_stylesheets_loaded ||= false
+  end
+
+  def calendar_date_select_javascripts_loaded?
+    @cds_javascripts_loaded ||= false
+  end
+
   # returns the selected calendar_date_select stylesheet (not an array)
   def calendar_date_select_stylesheets(options = {})
+    return "" if @cds_stylesheets_loaded
+
+    @cds_stylesheets_loaded = true
+
     options.assert_valid_keys(:style)
     "calendar_date_select/#{options[:style] || "default"}"
   end
 
   # returns an array of javascripts needed for the selected locale, date_format, and calendar control itself.
   def calendar_date_select_javascripts(options = {})
+    return [] if @cds_javascripts_loaded
+
+    @cds_javascripts_loaded = true
+
     options.assert_valid_keys(:locale)
     files = ["calendar_date_select/calendar_date_select"]
     files << "calendar_date_select/locale/#{options[:locale]}" if options[:locale]
@@ -16,9 +32,6 @@ module CalendarDateSelect::IncludesHelper
 
   # returns html necessary to load javascript and css to make calendar_date_select work
   def calendar_date_select_includes(*args)
-    return "" if @cds_already_included
-    @cds_already_included=true
-    
     options = (Hash === args.last) ? args.pop : {}
     options.assert_valid_keys(:style, :locale)
     options[:style] ||= args.shift
